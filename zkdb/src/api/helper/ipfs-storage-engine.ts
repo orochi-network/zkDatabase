ab7055f2b49d85265a026c51b9f5ce4c9849e577
Oleh Chausov <9oleg8@gmail.com>
2023-07-11 15:05:45 +0300

    Add data location to env

--- zkdb/src/api/helper/ipfs-storage-engine.ts
+++ zkdb/src/api/helper/ipfs-storage-engine.ts
@@ -1,10 +1,15 @@
+import config from './config.js';
 import { StorageEngineIPFS } from '../../storage-engine/index.js';
 
 const instanceStorage: { [key: string]: StorageEngineIPFS } = {};
 
 export const getStorageEngine = async () => {
   if (typeof instanceStorage.storage === 'undefined') {
-    instanceStorage.storage = await StorageEngineIPFS.getInstance('data');
+    let dataLocation = 'data';
+    if (config.dataLocation !== undefined) {
+      dataLocation = config.dataLocation;
+    }
+    instanceStorage.storage = await StorageEngineIPFS.getInstance(dataLocation);
     // Make sure metadata is created
     await instanceStorage.storage.tryResolve();
   }
