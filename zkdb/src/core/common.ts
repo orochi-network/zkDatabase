cfcc35b366875d27ebb8f7d5cc105153c557af9d
Chiro Hiro <chiro8x@gmail.com>
2023-07-21 00:50:22 +0700

    Add common interfaces

---
+++ zkdb/src/core/common.ts
@@ -0,0 +1,16 @@
+import { Field } from 'snarkyjs';
+
+export interface IKeyValue {
+  [key: string]: string;
+}
+
+export type TEntries = [string, string][];
+
+export interface IMerkleStorage {
+  [level: number]: { [node: string]: string };
+}
+
+export interface IDocument {
+  hash(): Field;
+  index(): IKeyValue;
+}
