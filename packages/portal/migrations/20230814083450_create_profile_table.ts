d6212ac2aad1a2d1123cac51620993649296e23c
dqtkien <dqtkien@gmail.com>
2023-08-15 21:56:15 +0700

    Fix migration file

--- packages/portal/migrations/20230814083450_create_profile_table.ts
+++ packages/portal/migrations/20230814083450_create_profile_table.ts
@@ -8,7 +8,7 @@ export async function up(knex: Knex): Promise<void> {
       addIdAndUuid(knex, table);
 
       table
-        .integer('userId')
+        .bigInteger('userId')
         .notNullable()
         .unsigned()
         .references('user.id')
