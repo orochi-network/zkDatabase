8ebb4e1dd3fe31ed893f69dd1a9f17e3d466a616
Chiro Hiro <chiro8x@gmail.com>
2023-10-29 13:04:38 +0700

    Add collection model

---
+++ packages/serverless/src/model/collection.ts
@@ -0,0 +1,56 @@
+import { CreateIndexesOptions, IndexSpecification } from 'mongodb';
+import { ModelBasic } from './abstract/basic';
+import { ModelDocument } from './document';
+
+export class ModelCollection extends ModelBasic {
+  private constructor(databaseName: string, collectionName: string) {
+    super(databaseName, collectionName);
+  }
+
+  public static getInstance(databaseName: string, collectionName: string) {
+    return new ModelCollection(databaseName, collectionName);
+  }
+
+  public getDocument() {
+    return ModelDocument.getInstance(this.databaseName!, this.collectionName!);
+  }
+
+  public async create(
+    indexSpecs: IndexSpecification,
+    indexOptions?: CreateIndexesOptions
+  ) {
+    await this.collection.createIndex(indexSpecs, indexOptions);
+  }
+
+  public async drop() {
+    return this.db.dropCollection(this.collectionName!);
+  }
+
+  public async list() {
+    return this.db.listCollections();
+  }
+
+  public async index(
+    indexSpec: IndexSpecification,
+    indexOptions?: CreateIndexesOptions
+  ) {
+    return this.collection.createIndex(indexSpec, indexOptions);
+  }
+
+  public async isIndexed(indexName: string): Promise<boolean> {
+    for await (const index of this.collection.listIndexes()) {
+      if (index.name === indexName) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public async dropIndex(indexName: string) {
+    return this.collection.dropIndex(indexName);
+  }
+
+  public async listIndexes() {
+    return this.collection.listIndexes();
+  }
+}
