b3aa3b4860e0fcfa563f0e78ce2362dc1aa7b3c1
Chiro Hiro <chiro8x@gmail.com>
2023-12-26 16:28:04 +0700

    Add basic setting to boot up project

--- packages/serverless/src/model/abstract/database-engine.ts
+++ packages/serverless/src/model/abstract/database-engine.ts
@@ -3,12 +3,18 @@ import logger from '../../helper/logger';
 
 export const ZKDATABASE_INDEX_RECORD = '_zkindex';
 export const ZKDATABASE_INDEX_COLLECTION = '_zkdatabase_index';
+// Group
 export const ZKDATABASE_GROUP_COLLECTION = '_zkdatabase_group';
 export const ZKDATABASE_GROUP_PERMISSION_COLLECTION =
-  '_zkdatabase_basic_permission';
+  '_zkdatabase_group_permission';
+// User -> Group mapping
 export const ZKDATABASE_USER_GROUP_COLLECTION = '_zkdatabase_user_group';
+// User permission
 export const ZKDATABASE_USER_PERMISSION_COLLECTION =
   '_zkdatabase_USER_permission';
+// Settings
+export const ZKDATABASE_SETTING_COLLECTION = '_zkdatabase_setting';
+// System management
 export const ZKDATABASE_MANAGEMENT_DB = '_zkdatabase_management';
 
 type ZKDatabaseIndex = {
