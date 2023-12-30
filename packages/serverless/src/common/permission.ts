43123381b4719483e59d29ad3bf29af4826919f5
Chiro Hiro <chiro8x@gmail.com>
2023-12-30 12:03:06 +0700

    Optimize permission storage with binary format

--- packages/serverless/src/common/permission.ts
+++ packages/serverless/src/common/permission.ts
@@ -1,9 +1,140 @@
+/* eslint-disable no-bitwise */
 export type PermissionRecord = {
+  // Permission to change permission and ownership
+  system: boolean;
+  // Permission to insert new record
   insert: boolean;
+  // Permission to read record
   read: boolean;
+  // Permission to write record
   write: boolean;
+  // Permission to delete record
   delete: boolean;
 };
+
+// All permission fields
+export const ZKDATABASE_ALL_PERMISSION: (keyof PermissionRecord)[] = [
+  'system' as keyof PermissionRecord,
+  'insert' as keyof PermissionRecord,
+  'read' as keyof PermissionRecord,
+  'write' as keyof PermissionRecord,
+  'delete' as keyof PermissionRecord,
+];
+
+// Binary form of permission
+export class PermissionBinary {
+  private permissionBinary: number;
+
+  private permission: PermissionRecord;
+
+  private constructor(permissionRecord: PermissionRecord) {
+    this.permission = permissionRecord;
+    this.permissionBinary =
+      PermissionBinary.toBinaryPermission(permissionRecord);
+  }
+
+  set system(value: boolean) {
+    this.set('system', value);
+  }
+
+  get system(): boolean {
+    return this.permission.system;
+  }
+
+  set insert(value: boolean) {
+    this.set('insert', value);
+  }
+
+  get insert(): boolean {
+    return this.permission.insert;
+  }
+
+  set read(value: boolean) {
+    this.set('read', value);
+  }
+
+  get read(): boolean {
+    return this.permission.read;
+  }
+
+  set write(value: boolean) {
+    this.set('write', value);
+  }
+
+  get write(): boolean {
+    return this.permission.write;
+  }
+
+  set delete(value: boolean) {
+    this.set('delete', value);
+  }
+
+  get delete(): boolean {
+    return this.permission.delete;
+  }
+
+  private set(field: keyof PermissionRecord, value: boolean) {
+    this.permission[field] = value;
+    if (value) {
+      this.permissionBinary = PermissionBinary.on(this.permissionBinary, field);
+    } else {
+      this.permissionBinary = PermissionBinary.off(
+        this.permissionBinary,
+        field
+      );
+    }
+  }
+
+  private static on(
+    binaryPermission: number,
+    field: keyof PermissionRecord
+  ): number {
+    return binaryPermission | (1 << ZKDATABASE_ALL_PERMISSION.indexOf(field));
+  }
+
+  private static off(
+    binaryPermission: number,
+    field: keyof PermissionRecord
+  ): number {
+    const bit = 1 << ZKDATABASE_ALL_PERMISSION.indexOf(field);
+    return (binaryPermission | bit) ^ bit;
+  }
+
+  public static toBinaryPermission(permissionRecord: PermissionRecord): number {
+    let permission = 0;
+    for (let i = 0; i < ZKDATABASE_ALL_PERMISSION.length; i += 1) {
+      permission |= permissionRecord[ZKDATABASE_ALL_PERMISSION[i]] ? 1 << i : 0;
+    }
+    return permission;
+  }
+
+  public static fromBinaryPermission(
+    permissionBinary: number
+  ): PermissionRecord {
+    const permission: PermissionRecord = {
+      system: false,
+      insert: false,
+      read: false,
+      write: false,
+      delete: false,
+    };
+    for (let i = 0; i < ZKDATABASE_ALL_PERMISSION.length; i += 1) {
+      permission[ZKDATABASE_ALL_PERMISSION[i]] =
+        (permissionBinary & (1 << i)) !== 0;
+    }
+    return permission;
+  }
+
+  public static fromBinary(number: number): PermissionBinary {
+    return new PermissionBinary(PermissionBinary.fromBinaryPermission(number));
+  }
+
+  public static fromRecord(
+    permissionRecord: PermissionRecord
+  ): PermissionBinary {
+    return new PermissionBinary(permissionRecord);
+  }
+}
 
 /**
  * Reducing array of permissions to single permission set
