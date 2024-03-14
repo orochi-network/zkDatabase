11fa3f3fa6ae367b366069caa254ed505be24b2b
magestrio <9oleg8@gmail.com>
2024-03-14 15:14:54 +0200

    Removed the createWithSchema method

--- packages/serverless/src/model/abstract/collection.ts
+++ packages/serverless/src/model/abstract/collection.ts
@@ -2,7 +2,6 @@ import { CreateIndexesOptions, IndexSpecification, Document } from 'mongodb';
 import { isOk } from '../../helper/common';
 import ModelBasic from './basic';
 import ModelDatabase from './database';
-import { SchemaDefinition } from '../common/schema';
 
 /**
  * Build on top of ModelBasic, it handle everything about collection in general
@@ -30,30 +29,11 @@ export class ModelCollection<T extends Document> extends ModelBasic<T> {
     return this.dbEngine.isCollection(this.databaseName!, this.collectionName!);
   }
 
-  public async createWithSchema(schema: SchemaDefinition) {
-    const indexSpecs: IndexSpecification = schema
-      .filter((field) => field.indexed)
-      .reduce(
-        (acc, field) => ({
-          ...acc,
-          [field.name]: 1,
-        }),
-        {}
-      );
-
-    if (Object.keys(indexSpecs).length > 0) {
-      this.create(indexSpecs);
-    }
-  }
-
   public async create(
     indexSpecs: IndexSpecification,
     indexOptions?: CreateIndexesOptions
   ) {
-    if (
-      this.databaseName &&
-      this.collectionName
-    ) {
+    if (this.databaseName && this.collectionName) {
       return new ModelCollection(
         this.databaseName,
         this.collectionName
