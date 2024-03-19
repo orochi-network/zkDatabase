56695cede3a933bba953127e316eba5981b4bbd8
magestrio <9oleg8@gmail.com>
2024-03-19 12:27:57 +0200

    Improved zk database global collection constants

--- packages/storage/src/common/const.ts
+++ packages/storage/src/common/const.ts
@@ -1,37 +1,40 @@
-// Schema metadata
-export const ZKDATABASE_SCHEMA_COLLECTION = '_zkdatabase_management';
+// Common constants for zkDatabase collections
+export const zkDatabaseCollections = {
+  // Schema metadata
+  schema: '_zkdatabase_management',
 
-// Group
-export const ZKDATABASE_GROUP_COLLECTION = '_zkdatabase_group';
+  // Group
+  group: '_zkdatabase_group',
 
-// User -> Group mapping
-export const ZKDATABASE_USER_GROUP_COLLECTION = '_zkdatabase_user_group';
+  // User -> Group mapping
+  userGroup: '_zkdatabase_user_group',
 
-// Permission
-export const ZKDATABASE_METADATA_COLLECTION = '_zkdatabase_permission';
+  // Permission
+  permission: '_zkdatabase_permission',
 
-// System management
-export const ZKDATABASE_GLOBAL_DB = '_zkdatabase_metadata';
+  // System management
+  globalMetadata: '_zkdatabase_metadata',
 
-// Database settings
-export const ZKDATABASE_DB_SETTING_COLLECTION = '_zkdatabase_db_setting';
+  // Database settings
+  dbSetting: '_zkdatabase_db_setting',
 
-// Merkle tree collection
-export const ZKDATABASE_MERKLE_TREE_COLLECTION = '_zkdatabase_merkle_tree';
+  // Merkle tree collection
+  merkleTree: '_zkdatabase_merkle_tree',
 
-// Proof queue
-export const ZKDATABASE_DB_QUEUE_COLLECTION = '_zkdatabase_proof_queue';
+  // Proof queue
+  proofQueue: '_zkdatabase_proof_queue',
 
-// Proof storage
-export const ZKDATABASE_DB_PROOF_COLLECTION = '_zkdatabase_proof_storage';
+  // Proof storage
+  proofStorage: '_zkdatabase_proof_storage',
+};
 
 // Metadata collections
-export const ZKDATABASE_METADATA = [
-  ZKDATABASE_SCHEMA_COLLECTION,
-  ZKDATABASE_GROUP_COLLECTION,
-  ZKDATABASE_USER_GROUP_COLLECTION,
-  ZKDATABASE_METADATA_COLLECTION,
-  ZKDATABASE_MERKLE_TREE_COLLECTION,
-  ZKDATABASE_DB_QUEUE_COLLECTION,
-  ZKDATABASE_DB_PROOF_COLLECTION,
+export const zkDatabaseMetadataCollections = [
+  zkDatabaseCollections.schema,
+  zkDatabaseCollections.group,
+  zkDatabaseCollections.userGroup,
+  zkDatabaseCollections.permission,
+  zkDatabaseCollections.merkleTree,
+  zkDatabaseCollections.proofQueue,
+  zkDatabaseCollections.proofStorage,
 ];
