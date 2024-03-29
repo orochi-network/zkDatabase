c55137065c3dc7e632743fe31d171707983256ae
magestrio <9oleg8@gmail.com>
2024-03-29 12:07:41 +0200

    Fixed ModelTask

--- packages/storage/src/database/global/queue.ts
+++ packages/storage/src/database/global/queue.ts
@@ -1,4 +1,4 @@
-import { InsertOneOptions } from 'mongodb';
+import { FindOptions, InsertOneOptions } from 'mongodb';
 import ModelBasic from '../base/basic.js';
 import { zkDatabaseConstants } from '../../common/const.js';
 
@@ -6,7 +6,7 @@ export type TaskEntity = {
   merkleIndex: bigint;
   hash: string;
   processed?: boolean;
-  createdAt?: Date;
+  createdAt: Date;
   database: string;
   collection: string;
 };
@@ -15,13 +15,19 @@ export class ModelTask extends ModelBasic<TaskEntity> {
   private static instance: ModelTask | null = null;
 
   private constructor() {
-    super(zkDatabaseConstants.globalDatabase, zkDatabaseConstants.globalCollections.queue);
+    super(
+      zkDatabaseConstants.globalDatabase,
+      zkDatabaseConstants.globalCollections.queue
+    );
   }
 
   public static getInstance(): ModelTask {
     if (!ModelTask.instance) {
       ModelTask.instance = new ModelTask();
-      ModelTask.instance.collection.createIndex({ merkleIndex: 1 }, { unique: true });
+      ModelTask.instance.collection.createIndex(
+        { merkleIndex: 1 },
+        { unique: true }
+      );
     }
     return ModelTask.instance;
   }
@@ -36,32 +42,31 @@ export class ModelTask extends ModelBasic<TaskEntity> {
     await this.collection.insertOne(
       {
         ...task,
-        createdAt: new Date(),
         processed: false,
       },
       options
     );
   }
 
-  public async getNewTask(): Promise<TaskEntity | null> {
+  public async getNewTask(options?: FindOptions): Promise<TaskEntity | null> {
     if (!this.collection) {
       throw new Error('TaskQueue is not connected to the database.');
     }
     const result = await this.collection.findOne(
       { processed: false },
-      { sort: { createdAt: 1 } }
+      { sort: { createdAt: 1 }, ...options }
     );
 
     const task = result as TaskEntity | null;
     return task;
   }
 
-  public async markTaskProcessed(taskId: bigint): Promise<void> {
+  public async markTaskProcessed(merkleIndex: bigint): Promise<void> {
     if (!this.collection) {
       throw new Error('TaskQueue is not connected to the database.');
     }
     await this.collection.updateOne(
-      { id: taskId },
+      { merkleIndex },
       { $set: { processed: true } }
     );
   }
