8092cac440efece06d92ff15c3b182b8a7fc3bce
magestrio <9oleg8@gmail.com>
2024-04-11 17:13:24 -0700

    Add schema validation for collection api

--- packages/serverless/src/apollo/app/collection.ts
+++ packages/serverless/src/apollo/app/collection.ts
@@ -4,14 +4,26 @@ import { ModelDatabase } from '@zkdb/storage';
 import {
   databaseName,
   collectionName,
-  permissionDetail,
-  schemaFields,
+  permissionDetail
 } from './common';
 import { TDatabaseRequest } from './database';
 import resolverWrapper from '../validation';
 import { PermissionsData } from '../types/permission';
 import { SchemaData } from '../types/schema';
 import { createCollection } from '../../domain/use-case/collection';
+import { O1JS_VALID_TYPE } from '../../common/const';
+
+export const schemaField = Joi.object({
+  name: Joi.string()
+    .pattern(/^[a-z][a-zA-Z0-9_]+$/)
+    .required(),
+  kind: Joi.string()
+    .valid(...O1JS_VALID_TYPE)
+    .required(),
+  indexed: Joi.boolean(),
+})
+
+export const schemaFields = Joi.array().items(schemaField);
 
 export type TCollectionRequest = TDatabaseRequest & {
   collectionName: string;
