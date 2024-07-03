6d69e817f2a1b171e1ea2e7c8d8a8d070cb1cb53
magestrio <9oleg8@gmail.com>
2024-07-03 10:33:09 +0300

    Improve the getNextTaskId method

--- packages/broker-service/src/domain/get-next-task.ts
+++ packages/broker-service/src/domain/get-next-task.ts
@@ -1,11 +1,7 @@
 import { ModelQueueTask } from "@zkdb/storage";
 
-export async function getNextTaskId(): Promise<string | null> {
-  const task =
-    await ModelQueueTask.getInstance().getLatestQueuedTaskByDatabase();
+export async function getNextTaskId(): Promise<string | undefined> {
+  const task = await ModelQueueTask.getInstance().getLatestQueuedTaskByDatabase();
 
-  if (task) {
-    return task._id.toString();
-  }
-  return null;
+  return task?._id?.toString();
 }
