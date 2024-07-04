b31bea5a6d4fe6423298d16f18ea54091fdf316f
magestrio <9oleg8@gmail.com>
2024-07-04 06:27:44 +0300

    Remove hardcode uri from ApolloClient

--- packages/api/src/graphql/client.ts
+++ packages/api/src/graphql/client.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
+import config from '../helper/config';
 
 const client = new ApolloClient({
-  uri: 'http://localhost:4000/graphql',
+  uri: config.aasUri,
   cache: new InMemoryCache(),
 });
 
