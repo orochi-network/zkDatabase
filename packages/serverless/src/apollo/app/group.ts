84a7a9a998e698d81cf17c90da80b96d5eafa2d4
magestrio <9oleg8@gmail.com>
2024-07-18 22:37:17 +0300

    Add create group endpoint

--- packages/serverless/src/apollo/app/group.ts
+++ packages/serverless/src/apollo/app/group.ts
@@ -1,10 +1,25 @@
 import Joi from 'joi';
 import GraphQLJSON from 'graphql-type-json';
+import { withTransaction } from '@zkdb/storage';
 import resolverWrapper from '../validation';
-import { databaseName, userName } from './common';
+import { databaseName, groupDescription, groupName, userName } from './common';
 import { TDatabaseRequest } from './database';
 import ModelGroup from '../../model/database/group';
 import ModelUserGroup from '../../model/database/user-group';
+import { TCollectionRequest } from './collection';
+import { createGroup } from '../../domain/use-case/group';
+import { AppContext } from '../../common/types';
+
+export type TGroupCreateRequest = TCollectionRequest & {
+  groupName: string;
+  groupDescription: string;
+};
+
+export const GroupCreateRequest = Joi.object<TGroupCreateRequest>({
+  databaseName,
+  groupName,
+  groupDescription,
+});
 
 export const typeDefsGroup = `#graphql
 scalar JSON
@@ -14,6 +29,14 @@ type Mutation
 extend type Query {
   groupListAll(databaseName: String!): [String]
   groupListByUser(databaseName: String!, userName: String!): [String]
+}
+
+extend type Mutation {
+  groupCreate(
+    databaseName: String!,
+    groupName: String!,
+    groupDescription: String
+  ): Boolean
 }
 `;
 
@@ -43,11 +66,28 @@ const groupListByUser = resolverWrapper(
   }
 );
 
+const groupCreate = resolverWrapper(
+  GroupCreateRequest,
+  async (_root: unknown, args: TGroupCreateRequest, ctx: AppContext) =>
+    withTransaction(async (session) =>
+      createGroup(
+        args.databaseName,
+        ctx.userName,
+        args.groupName,
+        args.groupDescription,
+        session
+      )
+    )
+);
+
 type TGroupResolver = {
   JSON: typeof GraphQLJSON;
   Query: {
     groupListAll: typeof groupListAll;
     groupListByUser: typeof groupListByUser;
+  };
+  Mutation: {
+    groupCreate: typeof groupCreate;
   };
 };
 
@@ -56,5 +96,8 @@ export const resolversGroup: TGroupResolver = {
   Query: {
     groupListAll,
     groupListByUser,
+  },
+  Mutation: {
+    groupCreate,
   },
 };
