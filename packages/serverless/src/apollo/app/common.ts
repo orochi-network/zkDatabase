4be46c2affa2c7b68a5453e1cb3a61e739dff176
magestrio <9oleg8@gmail.com>
2024-07-18 22:38:09 +0300

    the actual length of public key is 55

--- packages/serverless/src/apollo/app/common.ts
+++ packages/serverless/src/apollo/app/common.ts
@@ -50,9 +50,9 @@ export const groupDescription = Joi.string()
 
 export const publicKey = Joi.string()
   .trim()
-  .length(52)
+  .length(55)
   .required()
-  .pattern(/^[A-HJ-NP-Za-km-z1-9]{52}$/);
+  .pattern(/^[A-HJ-NP-Za-km-z1-9]{55}$/);
 
 export const indexName = Joi.string()
   .trim()
