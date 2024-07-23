e066441b9e17b04dcae1fee7ec8f37a1da8df814
magestrio <9oleg8@gmail.com>
2024-07-23 09:15:53 +0300

    expose new group api calls

--- packages/api/src/graphql/group/index.ts
+++ packages/api/src/graphql/group/index.ts
@@ -1,2 +1,6 @@
 export { listGroups } from "./query/list-group.js";
 export { listGroupsByUser } from "./query/list-group-user.js";
+export { getGroupDescription } from "./query/get-group-description.js";
+export { createGroup } from "./mutation/create-group.js";
+export { addUsersToGroup } from "./mutation/add-users-to-group.js";
+export { changeGroupDescription } from "./mutation/change-group-description.js";
