d9f9ab214236d39150a0ed589add3622bc8822a6
magestrio <9oleg8@gmail.com>
2024-07-29 11:36:12 +0300

    rename external signer to aurowallet signer for clarity

--- packages/zkdb/src/sdk/signer/signer.ts
+++ packages/zkdb/src/sdk/signer/signer.ts
@@ -1,11 +1,11 @@
 import { isBrowser } from "../../utils/environment.js";
-import { ExternalSigner } from "./browser-signer.js";
+import { AuroWalletSigner } from "./browser-signer.js";
 import { Signer } from "./interface/signer.js";
 
 let signer: Signer;
 
 export function setSigner(newSigner: Signer) {
-  if (signer instanceof ExternalSigner && !isBrowser()) {
+  if (signer instanceof AuroWalletSigner && !isBrowser()) {
     throw Error('External signer cannot be set in non-browser environment')
   }
   signer = newSigner;
