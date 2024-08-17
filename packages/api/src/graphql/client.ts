49fca3ef5b816d5309c4f9c661573b919b4ef6bd
magestrio <9oleg8@gmail.com>
2024-08-17 11:58:06 +0300

    remove typename apollo

--- packages/api/src/graphql/client.ts
+++ packages/api/src/graphql/client.ts
@@ -1,8 +1,9 @@
-import pkg from '@apollo/client';
+import pkg from "@apollo/client";
 const { ApolloClient, InMemoryCache, HttpLink, ApolloLink } = pkg;
-import { setContext } from '@apollo/client/link/context/index.js';
-import { getToken } from '../authentication/jwt-token.js';
-import { config } from '../helper/config.js';
+import { setContext } from "@apollo/client/link/context/index.js";
+import { removeTypenameFromVariables } from "@apollo/client/link/remove-typename/index.js";
+import { getToken } from "../authentication/jwt-token.js";
+import { config } from "../helper/config.js";
 
 const httpLink = new HttpLink({
   uri: config.AAS_URI,
@@ -13,16 +14,18 @@ const authLink = setContext(async (_, { headers }) => {
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
 
-const link = ApolloLink.from([authLink, httpLink]);
+const removeTypenameLink = removeTypenameFromVariables();
+
+const link = ApolloLink.from([removeTypenameLink, authLink, httpLink]);
 
 const client = new ApolloClient({
   link: link,
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({ addTypename: false }),
 });
 
 export default client;
