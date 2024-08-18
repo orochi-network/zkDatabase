2689bafd7ad48a2d889326a72f31f3b05ae06e5f
magestrio <9oleg8@gmail.com>
2024-08-19 01:06:23 +0300

    add searchUsers request

--- packages/api/src/graphql/user/index.ts
+++ packages/api/src/graphql/user/index.ts
@@ -2,10 +2,6 @@ import { signIn } from "./mutation/user-sign-in.js";
 import { signOut } from "./mutation/user-sign-out.js";
 import { signUp } from "./mutation/user-sign-up.js";
 import { getSignInData } from "./query/user-sign-in-data.js";
+import { searchUsers } from "./query/search-users.js";
 
-export {
-  signIn,
-  signOut,
-  signUp,
-  getSignInData
-}
+export { signIn, signOut, signUp, getSignInData, searchUsers };
