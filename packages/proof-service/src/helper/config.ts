ed629b4bf2d3afd6a52d523845627ada8edf5c3c
chiro-hiro <chiro8x@gmail.com>
2024-09-24 12:34:12 +0700

    Release all packages 1.1.x

--- packages/proof-service/src/helper/config.ts
+++ packages/proof-service/src/helper/config.ts
@@ -1,38 +1,29 @@
-/* eslint-disable no-param-reassign */
-import { ConfigLoader } from '@orochi-network/framework';
+import {
+  ConfigLoader,
+  NODE_ENV_VALUE,
+  TNodeEnv,
+} from '@orochi-network/framework';
 import Joi from 'joi';
 
-export const NODE_ENV_VALUES = [
-  'development',
-  'production',
-  'staging',
-] as const;
-
-type TNodeEnv = (typeof NODE_ENV_VALUES)[number];
-
-interface TApplicationConfig {
+type TApplicationConfig = {
   NODE_ENV: TNodeEnv;
   MONGODB_URL: string;
   BROKER_SERVICE: string;
-}
+};
 
-const configLoader = new ConfigLoader<TApplicationConfig>(
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  (raw: any) => raw,
-  {
-    NODE_ENV: Joi.string()
-      .required()
-      .trim()
-      .valid(...NODE_ENV_VALUES)
-      .default('production'),
-    MONGODB_URL: Joi.string()
-      .trim()
-      .required()
-      .regex(/^mongodb([+a-z]+|):\/\//),
-    BROKER_SERVICE: Joi.string()
-      .pattern(/^http(|s):\/\//)
-      .default('http://0.0.0.0:31337'),
-  }
-);
+const configLoader = new ConfigLoader<TApplicationConfig>((raw) => raw, {
+  NODE_ENV: Joi.string()
+    .required()
+    .trim()
+    .valid(...NODE_ENV_VALUE)
+    .default('production'),
+  MONGODB_URL: Joi.string()
+    .trim()
+    .required()
+    .regex(/^mongodb([+a-z]+|):\/\//),
+  BROKER_SERVICE: Joi.string()
+    .pattern(/^http(|s):\/\//)
+    .default('http://0.0.0.0:31337'),
+});
 
 export const { config } = configLoader;
