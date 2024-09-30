6b263db87d4bd4929c1ec516d1e94b6415ca1086
magestrio <9oleg8@gmail.com>
2024-09-30 11:23:47 +0300

    left todo

--- packages/serverless/src/domain/use-case/collection.ts
+++ packages/serverless/src/domain/use-case/collection.ts
@@ -169,6 +169,7 @@ async function dropIndex(
   if (
     await hasCollectionPermission(databaseName, collectionName, actor, 'system')
   ) {
+    // TODO: Allow people to choose the sorting order
     const index = `${indexName}_1`;
     if (await doesIndexExist(databaseName, actor, collectionName, index)) {
       return ModelCollection.getInstance(
