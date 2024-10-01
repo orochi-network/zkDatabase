a05c7f72d9ac191a5f4026d692f0596284b943f9
magestrio <9oleg8@gmail.com>
2024-10-01 06:33:41 +0300

    replace any with concrete type

--- packages/storage/src/database/general/collection.ts
+++ packages/storage/src/database/general/collection.ts
@@ -90,8 +90,11 @@ export class ModelCollection<T extends Document> extends ModelBasic<T> {
   }
 
   public async size(): Promise<number> {
-    return ((await this.db.command({ collStats: this.collectionName })) as any)
-      .size;
+    return (
+      (await this.db.command({ collStats: this.collectionName })) as {
+        size: number;
+      }
+    ).size;
   }
 }
 
