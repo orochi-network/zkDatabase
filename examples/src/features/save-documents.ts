5dcb718016f286c24462cf63839b1173227f94bc
magestrio <9oleg8@gmail.com>
2024-10-09 14:48:19 +0300

    uncomment deploy smart contract code

--- examples/src/features/save-documents.ts
+++ examples/src/features/save-documents.ts
@@ -45,12 +45,12 @@ async function run() {
 
   const zkDbPrivateKey = PrivateKey.fromBase58(ZKDB_PRIVATE_KEY);
 
-  // const tx = await zkdb
-  //   .fromBlockchain()
-  //   .deployZKDatabaseSmartContract(18, zkDbPrivateKey);
+  const tx = await zkdb
+    .fromBlockchain()
+    .deployZKDatabaseSmartContract(18, zkDbPrivateKey);
 
-  // console.log('deployment hash', tx.hash);
-  // await tx.wait();
+  console.log('deployment hash', tx.hash);
+  await tx.wait();
 
   await zkdb
     .fromGlobal()
