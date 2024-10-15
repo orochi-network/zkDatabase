c6876681b1b1ad4702f86fedf6538d762e8472bf
Trần Quang Sáng <tranquangsang12.7@gmail.com>
2024-10-15 10:23:02 +0700

    Feature: Adding dynamic import for nodejs modules

--- packages/smart-contract/src/helper/environment.ts
+++ packages/smart-contract/src/helper/environment.ts
@@ -5,3 +5,13 @@ export function isNode(): boolean {
     process.versions.node != null
   );
 }
+
+export async function getNodeDependencies() {
+  if (isNode()) {
+    const path = await import('path');
+    const ftp = await import('basic-ftp');
+    const fs = await import('fs/promises');
+    return { path, ftp, fs };
+  }
+  return null;
+}
