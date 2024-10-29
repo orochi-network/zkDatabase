7b85b9e136b29562e3a92d184e7f6c36d0070e02
Trần Quang Sáng <tranquangsang12.7@gmail.com>
2024-10-29 13:34:36 +0700

    Feature: Remove unused comment

--- packages/compile-service/src/service/zk-compile.ts
+++ packages/compile-service/src/service/zk-compile.ts
@@ -1,6 +1,6 @@
 import { logger } from "@helper";
 import { ZKDatabaseSmartContractWrapper } from "@zkdb/smart-contract";
-import { fetchAccount, Mina, PrivateKey, PublicKey } from "o1js";
+import { Mina, PrivateKey, PublicKey } from "o1js";
 import { DbDeployQueue } from "..";
 
 export class ZkCompileService {
@@ -10,19 +10,7 @@ export class ZkCompileService {
   async compileAndCreateUnsignTx(req: DbDeployQueue) {
     // Set active network
     const network = Mina.Network(this.network);
-    console.log(
-      "🚀 ~ ZkCompileService ~ compileAndCreateUnsignTx ~ this.network:",
-      this.network
-    );
     Mina.setActiveInstance(network);
-    // Checking the payer balance
-    // const payerAccount = await fetchAccount({
-    //   publicKey: PublicKey.fromBase58(req.payerAddress),
-    // });
-    // console.log(payerAccount.account?.balance);
-    // if (payerAccount.account?.balance.lessThan(UInt64.from(1))) {
-    //   throw new Error("Insufficient funds, need at least 1.1 Mina to deploy");
-    // }
     // Create keypair for zkApp contract
     const zkDbPrivateKey = PrivateKey.random();
     const zkDbPublicKey = PublicKey.fromPrivateKey(zkDbPrivateKey);
