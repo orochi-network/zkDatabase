e106a5391d94e6a86a9f86c791d06138595455a1
Trần Quang Sáng <tranquangsang12.7@gmail.com>
2024-11-03 23:53:10 +0700

    Feature: Add getInstance and type definition for rollup item

--- packages/storage/src/database/common/rollup.ts
+++ packages/storage/src/database/common/rollup.ts
@@ -3,14 +3,30 @@ import { DB } from '../../helper';
 import { zkDatabaseConstants } from '../../common';
 import ModelGeneral from '../base/general';
 
-export type RollupItem = {};
+export type RollupItem = {
+  txHash: string;
+  status: string; // TODO: Improve status
+  createdAt: string;
+  merkleRoot: string;
+  newMerkleRoot: string;
+  databaseName: string;
+  error?: string;
+};
+
 export class ModelRollup extends ModelGeneral<RollupItem> {
-  private instances = new Map<string, ModelRollup>();
+  private static instances = new Map<string, ModelRollup>();
   private constructor(databaseName: string) {
     super(
       databaseName,
       DB.service,
       zkDatabaseConstants.globalCollections.rollup
     );
+  }
+
+  static getInstance(databaseName: string) {
+    if (!ModelRollup.instances.has(databaseName)) {
+      ModelRollup.instances.set(databaseName, new ModelRollup(databaseName));
+    }
+    return ModelRollup.instances.get(databaseName);
   }
 }
