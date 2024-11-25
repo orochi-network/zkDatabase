a0855e7033d117611f23ccc0ab8319d150866a0e
Chiro Hiro <chiro8x@gmail.com>
2024-11-25 14:47:21 +0700

    Add method to connect zkdb via URL

--- packages/zkdb/src/sdk/global/zkdatabase-client.ts
+++ packages/zkdb/src/sdk/global/zkdatabase-client.ts
@@ -1,29 +1,82 @@
 import { ApiClient, IApiClient } from '@zkdb/api';
-import { Authenticator, ISecureStorage } from '../authentication';
+import { Authenticator } from '../authentication';
 import { ZKDatabase, GlobalContext } from '../interfaces';
 import { ZKDatabaseImpl, GlobalContextImpl } from '../impl';
 
-import { Signer } from '../signer';
+import { AuroWalletSigner, NodeSigner, Signer } from '../signer';
+import { PrivateKey } from 'o1js';
 
 export class ZKDatabaseClient {
   public apiClient: IApiClient;
 
   public authenticator: Authenticator;
 
-  private constructor(apiClient: IApiClient, authenticator: Authenticator) {
+  public minaRPC: string;
+
+  private constructor(
+    apiClient: IApiClient,
+    authenticator: Authenticator,
+    minaRPC: string
+  ) {
     this.apiClient = apiClient;
     this.authenticator = authenticator;
+    this.minaRPC = minaRPC;
   }
 
-  public static newInstance(
-    url: string,
-    signer: Signer,
-    storage: ISecureStorage
-  ) {
-    const apiClient = ApiClient.newInstance(url);
-    const authenticator = new Authenticator(signer, apiClient, storage);
-    apiClient.api.setContext(() => authenticator.getAccessToken());
-    return new ZKDatabaseClient(apiClient, authenticator);
+  /**
+   * Create new instance of ZKDatabaseClient by url
+   * Connect from NodeJS using a private key
+   * ```ts
+   * const client = await ZKDatabaseClient.connect('zzkdb+https://username@EKEGu8rTZbfWE1HWLxWtDnjt8gchvGxYM4s5q3KvNRRfdHBVe6UU:test-serverless.zkdatabase.org/graphql?db=my-db');
+   * ```
+   * Connect from browser using Auro Wallet
+   * ```ts
+   * const client = await ZKDatabaseClient.connect('zzkdb+https://username@test-serverless.zkdatabase.org/graphql?db=my-db');
+   * ```
+   * @param url
+   * @returns
+   */
+  public static async connec(url: string): Promise<ZKDatabaseClient> {
+    const urlInstance = new URL(url);
+    const { username, password, protocol, hostname, pathname, searchParams } =
+      urlInstance;
+    const [base, abstract] = protocol.replace(':', '').split('+');
+    if (base != 'zkdb') {
+      throw new Error('Invalid protocol');
+    }
+    const apiURL = `${abstract}://${hostname}/${pathname}`;
+    const db = searchParams.get('db');
+    if (!db) {
+      throw new Error('Database name is required');
+    }
+    const apiClient = ApiClient.newInstance(apiURL);
+    // Get envrionment variable
+    const envResult = await apiClient.environment.getEnvironment(undefined);
+    const { networkId, networkUrl } = envResult.isOne()
+      ? envResult.unwrap()
+      : {};
+    if (typeof networkId === 'string' && typeof networkUrl === 'string') {
+      if (password === '' || password === 'auro-wallet') {
+        const signer = new AuroWalletSigner();
+        const authenticator = new Authenticator(
+          signer,
+          apiClient,
+          global.localStorage
+        );
+        return new ZKDatabaseClient(apiClient, authenticator, networkUrl);
+      } else {
+        const signer = new NodeSigner(
+          PrivateKey.fromBase58(password),
+          networkId
+        );
+        return new ZKDatabaseClient(
+          apiClient,
+          new Authenticator(signer, apiClient),
+          networkUrl
+        );
+      }
+    }
+    throw new Error('Invalid environment');
   }
 
   public getSigner(): Signer {
