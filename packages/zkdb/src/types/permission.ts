7883c014e5be420e5480a42938c30e2054f2cc29
SangTran-127 <tranquangsang12.7@gmail.com>
2024-11-28 11:43:59 +0700

    Add on permission builder

--- packages/zkdb/src/types/permission.ts
+++ packages/zkdb/src/types/permission.ts
@@ -1,15 +1,48 @@
+import { Schema, ZKDatabaseClient } from '@sdk';
 import { PermissionRecord } from '../common/permission';
 
 export type PermissionGroup = 'User' | 'Group' | 'Other';
 
+export type TPermissionOption = {
+  write: boolean;
+  read: boolean;
+  create: boolean;
+  delete: boolean;
+};
+
+/**
+ * Permissions policy configuration can either be public, private, strict or custom.
+ */
 export type Permissions = {
   permissionOwner?: Partial<PermissionRecord>;
   permissionGroup?: Partial<PermissionRecord>;
   permissionOther?: Partial<PermissionRecord>;
 };
 
-export class AccessPermissions {
-  static readonly readOnlyPermissions: PermissionRecord = {
+
+// Implement builder pattern for pipeline chain style
+class PermissionBuilder {
+  constructor(private permission: Permissions) {}
+
+  group(permissionGroup: Partial<TPermissionOption>): PermissionBuilder {
+    this.permission = {...this.permission, ...permissionGroup}
+    return this
+  }
+
+  owner(ownerPermissions: Partial<TPermissionOption>) {
+    this.permission = { ...this.permission, ...ownerPermissions };
+    return this; 
+  }
+
+  other(otherPermissions: Partial<TPermissionOption>) {
+    this.permission = { ...this.permission, ...otherPermissions };
+    return this; 
+  }
+
+}
+
+export class AccessPermission {
+  private static readonly permissionReadOnly: PermissionRecord = {
     write: false,
     read: true,
     create: false,
@@ -17,7 +50,7 @@ export class AccessPermissions {
     system: false,
   };
 
-  static readonly fullAdminPermissions: PermissionRecord = {
+  private static readonly permissionFullSystem: PermissionRecord = {
     write: true,
     read: true,
     create: true,
@@ -25,7 +58,7 @@ export class AccessPermissions {
     system: true,
   };
 
-  static readonly noPermissions: PermissionRecord = {
+  private static readonly permissionNone: PermissionRecord = {
     write: false,
     read: false,
     create: false,
@@ -33,11 +66,45 @@ export class AccessPermissions {
     system: false,
   };
 
-  static readonly fullAccessPermissions: PermissionRecord = {
+  private static readonly permissionFull: PermissionRecord = {
     write: true,
     read: true,
     create: true,
     delete: true,
     system: false,
   };
+
+  // Policy
+  /**
+   * Public permissions allow full access to all entities.
+   */
+  static readonly policyPublic = new PermissionBuilder ({
+    permissionOwner: AccessPermission.permissionFullSystem,
+    permissionGroup: AccessPermission.permissionFull,
+    permissionOther: AccessPermission.permissionFull,
+  });
+
+  /**
+   * Private permissions restrict access for others, but allow full control to the owner and group.
+   */
+  static readonly policyPrivate = new PermissionBuilder( {
+    permissionOwner: AccessPermission.permissionFullSystem,
+    permissionGroup: AccessPermission.permissionFull,
+    permissionOther: AccessPermission.permissionNone,
+  })
+
+
+  /**
+   * Strict permissions limit access to the owner only, with no permissions for others or groups.
+   */
+  static readonly permissionStrict = new PermissionBuilder({
+    permissionOwner: AccessPermission.permissionFullSystem,
+    permissionGroup: AccessPermission.permissionNone,
+    permissionOther: AccessPermission.permissionNone,
+  });
 }
+
+
+const zkdb = await ZKDatabaseClient.connect('url')
+class Student extends Schema.create({})
+await zkdb.db('DB').collection('students').create('group', Student, [], AccessPermission.policyPrivate.group({create: true}))
