945e2f6f2a50fa9dd5f52cd0fce6a64658680553
Chiro Hiro <chiro8x@gmail.com>
2024-11-29 16:32:06 +0700

    Export type for permission and ownership

--- packages/permission/src/common.ts
+++ packages/permission/src/common.ts
@@ -1,6 +1,6 @@
-export const PERMISSION_TYPE = ['owner', 'group', 'other'] as const;
+export const OWNERSHIP_TYPE = ['owner', 'group'] as const;
 
-export type PermissionType = (typeof PERMISSION_TYPE)[number];
+export const PERMISSION_TYPE = ['owner', 'group', 'other'] as const;
 
 export const PERMISSION_FIELD = [
   'write',
@@ -10,13 +10,17 @@ export const PERMISSION_FIELD = [
   'system',
 ] as const;
 
+export type PermissionType = (typeof PERMISSION_TYPE)[number];
+
+export type OwnershipType = (typeof OWNERSHIP_TYPE)[number];
+
+export type PermissionRecordKey = (typeof PERMISSION_FIELD)[number];
+
 export const PERMISSION_ORDER_SYSTEM = 0;
 export const PERMISSION_ORDER_CREATE = 1;
 export const PERMISSION_ORDER_READ = 2;
 export const PERMISSION_ORDER_WRITE = 3;
 export const PERMISSION_ORDER_DELETE = 4;
-
-export type PermissionRecordKey = (typeof PERMISSION_FIELD)[number];
 
 export type PermissionRecord = {
   [k in PermissionRecordKey]: boolean;
@@ -28,4 +32,18 @@ export type PermissionDetail = {
 
 export type PermissionDetailPartial = {
   [key in PermissionType]: Partial<PermissionRecord>;
+};
+
+export type Ownership = {
+  [key in OwnershipType]: string;
+};
+
+export type OwnershipAndPermission = Ownership & {
+  permission: number;
+};
+
+export type OwnershipAndPermissionDetail = Ownership & {
+  owner: string;
+  group: string;
+  permission: PermissionDetail;
 };
