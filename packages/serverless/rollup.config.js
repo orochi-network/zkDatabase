06709acd6525b7a96a4a80326ba937c650089bac
SangTran-127 <tranquangsang12.7@gmail.com>
2024-12-17 11:33:45 +0700

    Add on rollup config file

--- packages/serverless/rollup.config.js
+++ packages/serverless/rollup.config.js
@@ -1,6 +1,8 @@
 import typescript from '@rollup/plugin-typescript';
 import alias from '@rollup/plugin-alias';
 import commonjs from '@rollup/plugin-commonjs';
+import nodeResolver from '@rollup/plugin-node-resolve';
+import json from '@rollup/plugin-json';
 
 export default {
   input: 'src/index.ts',
@@ -14,7 +16,7 @@ export default {
     alias({
       entries: [
         { find: '@', replacement: 'src' },
-        { find: '@apollo', replacement: 'src/apollo' },
+        { find: '@apollo-app', replacement: 'src/apollo' },
         { find: '@domain', replacement: 'src/domain' },
         { find: '@helper', replacement: 'src/helper' },
         { find: '@model', replacement: 'src/model' },
