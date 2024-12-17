30e42d5b15f82ba601798b2597aaae284ac1cdc1
SangTran-127 <tranquangsang12.7@gmail.com>
2024-12-17 10:40:42 +0700

    Remove junk comment

--- packages/serverless/src/domain/use-case/metadata.ts
+++ packages/serverless/src/domain/use-case/metadata.ts
@@ -35,9 +35,8 @@ export class Metadata {
         databaseName,
         collectionName,
         actor,
-        sudo: sudo || metadata.metadata,
         // We going to reuse metadata from above to minimize the number of queries.
-        // sudo: sudo || metadata,
+        sudo: sudo || metadata.metadata,
       },
       session
     );
