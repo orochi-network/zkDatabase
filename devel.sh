61abe4351f329cc08e1043e16e00362a766b6e9f
Chiro Hiro <chiro8x@gmail.com>
2024-12-19 19:30:32 +0700

    Add common packages to workspace package.json

--- devel.sh
+++ devel.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-ls
-PID_LS="$!"
-echo $PID_LS
+
+ROOT_DIR=`pwd`
+find `pwd` -name "node_modules" -exec rm -rf {} \;
+rm -rf yarn.lock
+yarn
+cd $ROOT_DIR/packages/permission && yarn build
+cd $ROOT_DIR/packages/common && yarn build
+cd $ROOT_DIR/packages/storage && yarn build
+cd $ROOT_DIR/packages/serverless && yarn build
+cd $ROOT_DIR/packages/api && yarn build
+cd $ROOT_DIR/packages/zkdb && yarn build
