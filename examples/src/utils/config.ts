3ad7eb4586905d802014c4d1d4bc88e003c143a3
wonrax <hahuylonghai2012@gmail.com>
2025-01-02 12:12:37 +0700

    Use test serverless URL instead of local

--- examples/src/utils/config.ts
+++ examples/src/utils/config.ts
@@ -3,5 +3,5 @@ import { PrivateKey } from 'o1js';
 export const DB_NAME = 'example-database';
 
 export const PRIVATE_KEY = PrivateKey.random().toBase58();
-export const SERVERLESS_HOST = 'zkdb-serverless.zenfactory.org/graphql';
+export const SERVERLESS_HOST = 'test-serverless.zkdatabase.org/graphql';
 export const ZKDB_URL = `zkdb+https://username:${PRIVATE_KEY}@${SERVERLESS_HOST}?db=${DB_NAME}`;
