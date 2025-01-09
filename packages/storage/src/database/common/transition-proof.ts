0d87a2bac86a81ce368ceadf93d035791a23d9d2
wonrax <hahuylonghai2012@gmail.com>
2025-01-09 16:26:04 +0700

    Fix transition proof model copy paste residuals

--- packages/storage/src/database/common/transition-proof.ts
+++ packages/storage/src/database/common/transition-proof.ts
@@ -4,9 +4,7 @@ import { zkDatabaseConstant } from '@common';
 import { DATABASE_ENGINE } from '@helper';
 import { TDbRecord, TMerkleProof } from '@zkdb/common';
 import { ClientSession, OptionalId } from 'mongodb';
-import { Field, Poseidon } from 'o1js';
 import { ModelGeneral } from '../base';
-import { ModelMetadataDatabase } from '../global';
 import { ModelCollection } from '../general';
 
 export type TTransitionProof = {
@@ -24,44 +22,21 @@ export class ModelTransitionProof extends ModelGeneral<
 > {
   private static instances = new Map<string, ModelTransitionProof>();
 
-  private zeroes: Field[] = [];
-
-  private _height: number = 0;
-
-  private generateZeroNode(height: number) {
-    const zeroes = new Array<Field>(height);
-    zeroes[0] = Field(0n);
-    for (let i = 1; i < height; i += 1) {
-      zeroes[i] = Poseidon.hash([zeroes[i - 1], zeroes[i - 1]]);
-    }
-
-    this.zeroes = zeroes;
-  }
-
-  private constructor(databaseName: string, height: number) {
+  private constructor(databaseName: string) {
     super(
       zkDatabaseConstant.globalTransitionProofDatabase,
       DATABASE_ENGINE.proofService,
       databaseName
     );
-    this._height = height;
-    this.generateZeroNode(this._height);
   }
 
   public static async getInstance(
     databaseName: string
   ): Promise<ModelTransitionProof> {
     if (!ModelTransitionProof.instances.has(databaseName)) {
-      const modelDatabaseMetadata = ModelMetadataDatabase.getInstance();
-      const metadataDatabase = await modelDatabaseMetadata.findOne({
-        databaseName,
-      });
-      if (!metadataDatabase) {
-        throw new Error(`Metadata of ${databaseName} has not been found.`);
-      }
       ModelTransitionProof.instances.set(
         databaseName,
-        new ModelTransitionProof(databaseName, metadataDatabase?.merkleHeight)
+        new ModelTransitionProof(databaseName)
       );
       ModelTransitionProof.init(databaseName);
     }
@@ -77,7 +52,7 @@ export class ModelTransitionProof extends ModelGeneral<
     if (!(await collection.isExist())) {
       // TODO: are there any other indexes that need to be created?
       await collection.createSystemIndex(
-        { level: 1, index: 1 },
+        { operationNumber: 1 },
         { unique: true, session }
       );
       await collection.addTimestampMongoDb({ session });
