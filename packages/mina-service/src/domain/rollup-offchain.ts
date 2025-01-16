6f33623b1af2350551b892be8bc7825353071f53
SangTran-127 <tranquangsang12.7@gmail.com>
2025-01-16 14:52:16 +0700

    Spliting logic code to update and init

--- packages/mina-service/src/domain/rollup-offchain.ts
+++ packages/mina-service/src/domain/rollup-offchain.ts
@@ -1,5 +1,9 @@
 import { deserializeTransition } from '@helper';
-import { TRollUpOffChainRecord, TRollupQueueData } from '@zkdb/common';
+import {
+  TRollUpOffChainRecord,
+  TRollupQueueData,
+  TTransitionLogRecord,
+} from '@zkdb/common';
 import { ZkDbProcessor } from '@zkdb/smart-contract';
 import {
   ModelMetadataDatabase,
@@ -8,28 +12,36 @@ import {
   TCompoundSession,
 } from '@zkdb/storage';
 import { OptionalId } from 'mongodb';
-import { MerkleTree } from 'o1js';
 
 const MAX_MERKLE_TREE_HEIGHT = 256;
 const MIN_MERKLE_TREE_HEIGHT = 8;
 
+type TRollupInitParam = {
+  task: TRollupQueueData;
+  merkleHeight: number;
+  transitionLog: TTransitionLogRecord;
+};
+
+type TRollupUpdateParam = TRollupInitParam & {
+  previousZkProof: TRollUpOffChainRecord;
+};
+
 export class RollupOffChain {
-  static async create(
+  public static async rollup(
     task: TRollupQueueData,
     session: TCompoundSession
   ): Promise<OptionalId<TRollUpOffChainRecord>> {
     const { serverless, proofService } = session;
-    const { databaseName, transitionLogObjectId, operationNumber } = task;
 
     const imMetadataDatabase = ModelMetadataDatabase.getInstance();
 
     const metadataDatabase = await imMetadataDatabase.findOne(
-      { databaseName },
+      { databaseName: task.databaseName },
       { session: serverless }
     );
 
     if (!metadataDatabase) {
-      throw new Error(`Metadata database ${databaseName} cannot be found`);
+      throw new Error(`Metadata database ${task.databaseName} cannot be found`);
     }
 
     const { merkleHeight } = metadataDatabase;
@@ -43,91 +55,118 @@ export class RollupOffChain {
       );
     }
 
-    // ZkDbProcessor will automatically compile when getInstance
-    const zkAppProcessor = await ZkDbProcessor.getInstance(merkleHeight);
+    const imTransitionLog = await ModelTransitionLog.getInstance(
+      task.databaseName,
+      proofService
+    );
+
+    const transitionLog = await imTransitionLog.findOne({
+      _id: task.transitionLogObjectId,
+    });
+
+    if (!transitionLog) {
+      throw new Error(
+        `Cannot found rollup transaction for database ${task.databaseName}`
+      );
+    }
 
     const imRollupOffChain = ModelRollupOffChain.getInstance();
 
     // Get previous proof to update
     // NOTE: It must be sequential and can't be access with another queue task in the same database
-    const previousProof = await imRollupOffChain.findOne(
-      { databaseName },
+    const previousZkProof = await imRollupOffChain.findOne(
+      { databaseName: task.databaseName },
       { sort: { step: -1 }, session: proofService }
     );
 
-    const imTransitionLog = await ModelTransitionLog.getInstance(
-      databaseName,
-      proofService
-    );
+    if (!previousZkProof) {
+      // If previous proof not found and operationNumber must be 1, which mean first time create
+      const zkProof = await RollupOffChain.init({
+        task,
+        merkleHeight,
+        transitionLog,
+      });
 
-    const rollupTransition = await imTransitionLog.findOne({
-      _id: transitionLogObjectId,
+      return zkProof;
+    }
+
+    const zkProof = await RollupOffChain.update({
+      task,
+      merkleHeight,
+      transitionLog,
+      previousZkProof,
     });
 
-    if (!rollupTransition) {
-      throw new Error(
-        `Cannot found rollup transaction for database ${databaseName}`
-      );
-    }
+    return zkProof;
+  }
 
-    if (!previousProof) {
-      // If previous proof not found and operationNumber must be 1, which mean first time create
+  private static async init(
+    param: TRollupInitParam
+  ): Promise<OptionalId<TRollUpOffChainRecord>> {
+    const { task, merkleHeight, transitionLog } = param;
+    // ZkDbProcessor will automatically compile when getInstance
+    const zkAppProcessor = await ZkDbProcessor.getInstance(merkleHeight);
 
-      const merkleTree = new MerkleTree(merkleHeight);
-      const firstRollup = deserializeTransition(rollupTransition);
+    const transitionDeserialized = deserializeTransition(transitionLog);
 
-      // Output step increase by 1 after init 0 - 1
-      const firstRollupProof = await zkAppProcessor.init(
-        merkleTree.getRoot(),
-        merkleTree.getWitness(0n),
-        firstRollup.leafNew
-      );
-
-      const {
-        proof,
-        merkleRootOld,
-        step: outputStep,
-      } = zkAppProcessor.serialize(firstRollupProof);
-      // After init, output step must be 1n and equals to operationNumber 1n, throw Error if not
-      if (outputStep !== operationNumber) {
-        throw new Error(
-          `Output first step and operationNumber did not match. Except ${operationNumber} but received ${outputStep}`
-        );
-      }
-
-      return {
-        databaseName,
-        merkleRootOld,
-        proof,
-        step: outputStep,
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        transitionLogObjectId,
-      };
-    }
-    // Previous output step + 1n = operationNumber, if not throw Error
-    if (previousProof.step + 1n !== operationNumber) {
-      throw new Error(
-        `Previous output step and operationNumber did not match. Except ${operationNumber} but received ${previousProof.step}`
-      );
-    }
-
-    const previousProofFormat = await zkAppProcessor.deserialize(previousProof);
-
-    const newRollupProof = await zkAppProcessor.update(
-      previousProofFormat,
-      deserializeTransition(rollupTransition)
+    const zkProof = await zkAppProcessor.init(
+      transitionDeserialized.merkleRootNew,
+      transitionDeserialized.merkleProof,
+      transitionDeserialized.leafNew
     );
 
-    const { proof, merkleRootOld, step } =
-      zkAppProcessor.serialize(newRollupProof);
+    // Serialized after init
+    const serializedZkProof = zkAppProcessor.serialize(zkProof);
+    // After init, output step must be 1n and equals to operationNumber 1n, throw Error if not
+    if (serializedZkProof.step !== task.operationNumber) {
+      throw new Error(
+        `Output first step and operationNumber did not match. Except ${task.operationNumber} but received ${serializedZkProof.step}`
+      );
+    }
 
     return {
-      databaseName,
-      merkleRootOld,
-      proof,
-      transitionLogObjectId,
-      step,
+      databaseName: task.databaseName,
+      transitionLogObjectId: transitionLog._id,
+      merkleRootOld: serializedZkProof.merkleRootOld,
+      proof: serializedZkProof.proof,
+      step: serializedZkProof.step,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    };
+  }
+
+  private static async update(
+    param: TRollupUpdateParam
+  ): Promise<OptionalId<TRollUpOffChainRecord>> {
+    const { task, previousZkProof, merkleHeight, transitionLog } = param;
+
+    // Previous output step + 1n = operationNumber, if not throw Error
+    if (previousZkProof.step + 1n !== task.operationNumber) {
+      throw new Error(
+        `Previous output step and operationNumber did not match. Except ${task.operationNumber} but received ${previousZkProof.step}`
+      );
+    }
+    // ZkDbProcessor will automatically compile when getInstance
+    const zkAppProcessor = await ZkDbProcessor.getInstance(merkleHeight);
+
+    const transitionDeserialized = deserializeTransition(transitionLog);
+
+    const previousProofFormat =
+      await zkAppProcessor.deserialize(previousZkProof);
+
+    const zkProof = await zkAppProcessor.update(
+      previousProofFormat,
+      transitionDeserialized
+    );
+
+    const zkProofSerialized = zkAppProcessor.serialize(zkProof);
+
+    return {
+      databaseName: task.databaseName,
+      transitionLogObjectId: transitionLog._id,
+      merkleRootOld: zkProofSerialized.merkleRootOld,
+      proof: zkProofSerialized.proof,
+      step: zkProofSerialized.step,
       createdAt: new Date(),
       updatedAt: new Date(),
     };
