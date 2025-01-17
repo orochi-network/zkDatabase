05386e295a69c5fe7ce15b11a37e84a08c280621
wonrax <hahuylonghai2012@gmail.com>
2025-01-17 14:23:50 +0700

    Remove obsolete doc comment

--- packages/storage/src/database/common/sequencer.ts
+++ packages/storage/src/database/common/sequencer.ts
@@ -5,8 +5,6 @@ import { ClientSession, WithoutId } from 'mongodb';
 import { ModelBasic } from '../base';
 import { ModelCollection } from '../general';
 
-// TODO: sequence value should be bigint, not number, e.g. merkle index is bigint
-
 export class ModelSequencer extends ModelBasic<WithoutId<TSequencedItem>> {
   public static readonly INITIAL_SEQUENCE_VALUE = 1n;
   public static readonly SEQUENCE_INCREMENT = 1n;
