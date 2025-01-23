9dac232b9450d8f83b894f880a8624a697feead0
wonrax <hahuylonghai2012@gmail.com>
2025-01-23 14:22:37 +0700

    Change documentObjectIdPrevius and Current's types to ObjectId

--- packages/common/src/types/queue.ts
+++ packages/common/src/types/queue.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb';
 import { TNullable } from './common';
 
 export enum EDocumentOperation {
@@ -12,8 +13,8 @@ export type TDocumentQueuedData = {
   newDocumentHash?: string;
   merkleIndex: bigint;
   docId: string;
-  documentObjectIdPrevious: string | null;
-  documentObjectIdCurrent: string | null;
+  documentObjectIdPrevious: ObjectId | null;
+  documentObjectIdCurrent: ObjectId | null;
 };
 
 export enum EQueueTaskStatus {
