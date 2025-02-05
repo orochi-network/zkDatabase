2d625c144be57b71ae3d62d3c4f9eec8646516d5
SangTran-127 <tranquangsang12.7@gmail.com>
2025-02-05 11:57:54 +0700

    Add on zk proof example

--- examples/src/connection.ts
+++ examples/src/connection.ts
@@ -6,8 +6,8 @@ export const zkdb = await ZkDatabase.connect({
   userName: 'chiro-user',
   privateKey: 'EKFTciRxyxshZjimay9sktsn7v5PvmC5zPq7q4JnitHUytxUVnFP',
   environment: 'node',
-  // This URL is for local environment
-  url: 'http://0.0.0.0:4000/graphql',
+  // This URL is for test environment
+  url: 'https://test-serverless.zkdatabase.org/graphql',
 });
 
 export default zkdb;
--- examples/src/zk-proof.ts
+++ examples/src/zk-proof.ts
@@ -1,4 +1,10 @@
+import { verify } from 'o1js';
 import { zkdb } from './connection';
+
+// Check user existence then create
+if (!(await zkdb.auth.isUserExist('chiro-user'))) {
+  await zkdb.auth.signUp('chiro@orochi.network');
+}
 
 // Sign in
 await zkdb.auth.signIn();
@@ -6,9 +12,40 @@ await zkdb.auth.signIn();
 // Create new instance of `db_test`
 const dbTest = zkdb.db('db_test');
 
-console.log('ZK Proof:', await dbTest.zkProof());
+/**
+ * To verify zkProof, you have 2 options
+ * 1. Using our `zkProofVerify()`
+ * 2. Using o1js `verify()`
+ */
 
-console.log(await dbTest.zkProofStatus());
+// 1. Using zkdb `zkProofVerify` method
+/**
+ * Basically it a wrapper that handle
+ * get verification key, get zkProof
+ * and verify from o1js to user
+ * You just simply call it
+ * */
+console.log(await dbTest.zkProofVerify());
+
+// 2. Using o1js `verify()`
+/**
+ * To verify using o1js, all you need is `zkProof` and `verificationKey`
+ * Firstly, get verification key by calling `verificationKey()`
+ * Secondly, get zkProof by calling `zkProof()`
+ * Lastly, call the `verify()` from o1js
+ * */
+
+// Get verification key to verify
+const verificationKey = await dbTest.verificationKey();
+
+// Get zkProof to verify the proof
+const zkProof = await dbTest.zkProof();
+
+// Make sure we have both verification zkProof
+if (verificationKey && zkProof) {
+  // Verify proof via `verify` method from o1js
+  console.log('Is proof valid:', await verify(zkProof.proof, verificationKey));
+}
 
 // Sign out
 await zkdb.auth.signOut();
